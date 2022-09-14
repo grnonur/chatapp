683a1768646608f07138c7b3ccf87c1fdf6837df
grnonur <greenpikachuu@gmail.com>
2022-09-14 21:24:21 +0300

    Chat app

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from "dotenv"
+import { Server } from 'socket.io';
+
+dotenv.config();
+const port = process.env.PORT;
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//middlewares
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const server = app.listen(port, (req, res) => {
+    console.log(`Sunucu Aktif! http://127.0.0.1:${port}`);
+})
+
+const io = new Server(server);
+
+io.on('connection', socket => {
+    console.log(socket.id);
+
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data)
+    })
+})
